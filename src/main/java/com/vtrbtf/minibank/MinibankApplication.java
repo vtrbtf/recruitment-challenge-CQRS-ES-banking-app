1e41f5d912e2f0560ccf252782cbc9e7c6bc9e0d
vtrbtf <vitor.tfreitas@gmail.com>
2017-10-25 09:29:05 -0200

    Basic project structure

---
+++ src/main/java/com/vtrbtf/minibank/MinibankApplication.java
@@ -0,0 +1,12 @@
+package com.vtrbtf.minibank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinibankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinibankApplication.class, args);
+	}
+}
